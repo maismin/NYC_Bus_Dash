6da9fc38d2df5b297a1532b190ebe80cc19a0f0a
Simon Mai <maismin@gmail.com>
2018-04-25 03:56:20 -0400

    Change code to import from Components folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'; 
 import './App.css';
-import BusMapContainer from './BusMapContainer'
+import BusMapContainer from './Components/BusMapContainer';
+import FilterContainer from './Components/FilterContainer';
+import { Grid, Row, Col } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <BusMapContainer />
+        <Grid>
+          <Row className="show-grid" >
+            <Col md={12}>
+              <FilterContainer />
+            </Col>
+          </Row>
+
+          <Row className="show-grid" >
+            <Col md={12}>
+              
+            </Col>
+          </Row>
+
+        </Grid>
       </div>
     );
   }
